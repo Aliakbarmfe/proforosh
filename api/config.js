01858a071ccfb6697250adae6dcf8e75853e8adc
Aliakbarmfe <aliakbarmfe@gmail.com>
2026-08-24 01:08:50 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -2,12 +2,18 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCd_Ct5YY_3XiOjiunPGXNIro41EO3UfPM",
   authDomain: "proforosh.firebaseapp.com",
+  databaseURL: "https://proforosh-default-rtdb.firebaseio.com",
   projectId: "proforosh",
   storageBucket: "proforosh.firebasestorage.app",
   messagingSenderId: "646604737860",
-  appId: "1:646604737860:web:5eaede6fa3c894b669db11",
-  databaseURL: "https://proforosh-default-rtdb.firebaseio.com" // آدرس Realtime Database پروژه شما
+  appId: "1:646604737860:web:5eaede6fa3c894b669db11"
 };
+
+// مقداردهی اولیه امن فایربیس
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+const db = firebase.database();
 
 // کلید API مربوط به ImgBB
 const IMGBB_API_KEY = "32b44cf4a92e03d876d7d02104feabfb";
@@ -25,7 +31,7 @@ async function uploadToImgBB(file) {
 
     const data = await response.json();
     if (data && data.success) {
-      return data.data.url; // آدرس مستقیم عکس آپلود شده
+      return data.data.url;
     } else {
       throw new Error(data.error ? data.error.message : "خطا در آپلود تصویر");
     }
